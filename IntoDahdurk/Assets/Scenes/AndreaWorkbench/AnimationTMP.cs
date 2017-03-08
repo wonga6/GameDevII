d14fde5ca520db8dc5a18372008fb2e7bcb56622
Andy <Mijumaru13@gmail.com>
2017-03-08 01:00:28 -0500

    General maze set up. Import and clean up

---
+++ IntoDahdurk/Assets/Scenes/AndreaWorkbench/AnimationTMP.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTMP : MonoBehaviour {
+
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+
+		anim.Play("Walk");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
