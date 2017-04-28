c32e128846d6d0a4e877ff0ea6b751850904fa1b
Robert Eldredge <reldredge10@gmail.com>
2017-04-27 20:45:14 -0400

    Backup Scene created.  Also started work on better network manager

---
+++ IntoDahdurk/Assets/Scripts/MatchMaker.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Networking.NetworkSystem;
+using UnityEngine.Networking.Match;
+using UnityEngine.Networking.Types;
+using UnityEngine.SceneManagement;
+
+public class MatchMaker : MonoBehaviour {
+
+	private NEWNetworkManagerOverride networkManager;
+	private MatchInfo matchInfo;
+
+	void Start()
+	{
+		networkManager = GetComponent<NEWNetworkManagerOverride> ();
+		networkManager.StartMatchMaker ();
+	}
+
+
+	void Update()
+	{
+		if(Input.GetKeyDown("m"))
+			networkManager.matchMaker.ListMatches(0, 10, "", true, 0, 0, CountMatches);
+	}
+
+	void CountMatches(bool success, string extendedInfo, List<MatchInfoSnapshot> matches)
+	{
+		Debug.Log ("here");
+		if(success)
+		{
+			if (matches.Count != 0)
+			{
+				Debug.Log ("List was returned");
+				//join last server(Temporary)
+				Debug.Log("Open Matches = " + matches.Count);
+			}
+			else
+			{
+				Debug.Log ("No matches in room!");
+			}
+		}
+		else
+		{
+			Debug.LogError ("Could not connect to matchmaker");
+		}
+	}
+
+
+
+	//sets up match with host's info
+	public void CreateInternetMatch(string hostName)
+	{
+		networkManager.matchMaker.CreateMatch (hostName, 2, true, "", "", "", 0, 0, OnInternetMatchCreate);
+	}
+
+	void OnInternetMatchCreate(bool success, string extendedInfo, MatchInfo MI)
+	{
+		if (success) {
+			Debug.Log ("Match Created");
+			MatchInfo hostInfo = MI;
+			NetworkServer.Listen (hostInfo, 9000);
+			matchInfo = MI;
+			networkManager.StartHost (hostInfo);
+		} else 
+		{
+			Debug.LogError ("failed to create match");
+		}
+	}
+
+
+	//finds a match for client
+	public void FindInternetMatch()
+	{
+		networkManager.matchMaker.ListMatches (0, 10, "", true, 0, 0, OnInternetMatchList);
+	}
+
+	void OnInternetMatchList(bool success, string extendedInfo, List<MatchInfoSnapshot> matches)
+	{
+		if(success)
+		{
+			if (matches.Count != 0)
+			{
+				Debug.Log ("List was returned");
+				//join last server(Temporary)
+				networkManager.matchMaker.JoinMatch(matches[matches.Count-1].networkId, "", "", "", 0, 0, OnJoinInternetMatch);
+			}
+			else
+			{
+				Debug.Log ("No matches in room!");
+			}
+		}
+		else
+		{
+			Debug.LogError ("Could not connect to matchmaker");
+		}
+	}
+
+
+	//sets up client to host's instance
+	void OnJoinInternetMatch(bool success, string extendedInfo, MatchInfo MI)
+	{
+		if (success) 
+		{
+			Debug.Log ("Able to Join Match");
+			MatchInfo hostInfo = MI;
+			matchInfo = MI;
+			networkManager.StartClient (hostInfo);
+		} 
+		else 
+		{
+			Debug.LogError ("Match Join Failed");
+		}
+	}
+
+
+	public void disconnectPlayer(){
+		networkManager.matchMaker.SetMatchAttributes (matchInfo.networkId, false, matchInfo.domain, OnAttributesStopHost);
+	}
+
+	void OnAttributesStopHost(bool success, string extendedInfo)
+	{
+		/*if (success) 
+		{
+			Debug.Log ("Dropping Match");
+			networkManager.StopHost ();
+
+		} 
+		else 
+		{
+			Debug.LogError ("Failed to unlist");
+		}*/
+
+		//networkManager.StopHost ();
+		//Destroy (this.gameObject);
+		networkManager.matchMaker.DropConnection (matchInfo.networkId , matchInfo.nodeId, matchInfo.domain, OnDropConnection);
+	}
+
+	void OnDropConnection(bool success, string extendedInfo)
+	{
+		//networkManager.dontDestroyOnLoad = false;
+		SceneManager.LoadScene("Lobby");
+	}
+}
--- IntoDahdurk/Assets/Scripts/NEWNetworkManagerOverride.cs
+++ IntoDahdurk/Assets/Scripts/NEWNetworkManagerOverride.cs
@@ -1,19 +1,21 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.Networking.NetworkSystem;
-using UnityEngine.UI;
+using UnityEngine.Networking.Match;
+
 
 // copied from: https://forum.unity3d.com/threads/using-multiple-player-prefabs-with-the-network-manager.438930/
 
 // custom NetworkManager class to allow multiple player prefabs to be spawned
 // spawning of objects that arent' the players' now controlled by SpawnObjs.cs
-public class NetworkManagerOverride : NetworkManager
-{
+public class NEWNetworkManagerOverride : NetworkManager {
+
 	// PUBLIC VARIABLES
 	public int chosenPrefab = 0;
 	public GameObject[] playerPrefabs;
 	public Vector3[] playerStartPos;
-	//public BuildMaze bm;
 
 	// PRIVATE VARIABLES
 	int index = 0; // index for which gameobject from plaerPrefabs array to spawn
@@ -25,34 +27,37 @@ public class NetworkManagerOverride : NetworkManager
 		public int chosenClass;
 	}
 
+
 	// FUNCTIONS
 	#region Unity Functions
 
 	//When game starts, set max players to 2
-	public void Awake()
+	public void Start()
 	{
 		this.maxConnections = 2;
 	}
-		
+
 	// override network manager's OnServerAddPlayer so that multiple player prefabs can be added
 	public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId, NetworkReader extraMessageReader)
 	{
+		//Debug.Log ("add player");
 		GameObject player;
-		chosenPrefab = index;
+		//chosenPrefab = index;
 
 		//spawn player at start position based on index #
 		player = Instantiate(playerPrefabs[index], playerStartPos[index], Quaternion.identity) as GameObject;
-		//bm.SetWalls (index);
+
 		// add player to server
 		NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
 
 		//increment index
-		index++;
+		//index++;
 	}
 
 	// override network manager's OnClientConnect
 	public override void OnClientConnect(NetworkConnection conn)
 	{
+		Debug.Log ("Client connect");
 		//if there are more than 2 connections, cancel this connection
 		if (NetworkServer.connections.Count > 2) 
 		{
@@ -74,5 +79,17 @@ public class NetworkManagerOverride : NetworkManager
 	{
 		//base.OnClientSceneChanged(conn);
 	}
+
+
+	public override void OnStartHost()
+	{
+		index = 0;
+	}
+
+	public override void OnMatchJoined(bool success, string extendedInfo, MatchInfo matchInfo)
+	{
+		index = 1;
+	}
+
 	#endregion
 }
