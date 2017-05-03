b04e6d06c6a5e96a9f7726adaa17a61219b61bd1
Andy <awong7133@gmail.com>
2017-05-03 10:35:16 -0400

    Last changes to game - hacky game over, Dolos dialogue added. Note passed "fixed"?

---
+++ IntoDahdurk/Assets/Scripts/DolosManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class DolosManager: NetworkBehaviour {
+
+	// PUBLIC VARIABLES
+	public string path;
+
+	// PRIVATE VARIABLES
+	private int index = 0;
+	private List<string> text;
+	private List<GameObject> players;
+
+	// FUNCTIONS
+
+	#region Unity Functions
+	// Use this for initialization
+	void Start () {
+		text = new List<string> ();
+		players = new List<GameObject> ();
+
+		readTextFile ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	#endregion
+
+	#region Public Functions
+	public void showSpeech() {
+		if (index >= text.Count) {
+			return;
+		}
+
+		for(int i = 0; i < players.Count; i++) {
+			players [i].GetComponent<DolosNotes> ().RpcShowNote (text [index]);
+		}
+
+	}
+
+	public void increment() {
+		index++;
+	}
+
+	public void addPlayer(GameObject player) {
+		players.Add (player);
+	}
+	#endregion
+
+	#region Private Functions
+	// read in the text file of dolos's speech
+	private void readTextFile() {
+		StreamReader instream = new StreamReader (path);
+
+		while (!instream.EndOfStream) {
+			string line = instream.ReadLine ();
+			text.Add (line);
+		}
+
+
+		/* DEBUGGING INPUT */
+		for(int i = 0; i < text.Count; i++) {
+			Debug.Log (text [i]);
+		}
+		Debug.Log ("======");
+	}
+	#endregion
+}
---
+++ IntoDahdurk/Assets/Scripts/End.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class End : NetworkBehaviour {
+
+	// PRIVATE VARIABLES
+	private GameObject teleportPoint;
+	private GameObject gameOver;
+
+	// FUNCTIONS
+
+	#region Unity Functions
+	// Use this for initialization
+	void Start () {
+		teleportPoint = GameObject.Find ("TeleportPoint");	
+		gameOver = GameObject.Find ("EndGameManager");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// keyboard shortcut is for easy testing and showing purposes
+		// ideally should be removed when no longer needed
+		if(Input.GetKeyDown(KeyCode.E)) {
+			Vector3 newPosition = new Vector3 (teleportPoint.transform.position.x, 
+				transform.position.y, teleportPoint.transform.position.z);
+			transform.position = newPosition;
+		}
+	}
+
+	void OnCollisionEnter(Collision col) {
+		if(col.gameObject.tag == "fake" && gameOver != null) {
+			gameOver.GetComponent<ToGameOver> ().gameOver ();
+		}
+		else if(col.gameObject.tag == "tablet" && gameOver != null) {
+			gameOver.GetComponent<ToGameOver> ().endGame ();
+		}
+	}
+	#endregion
+}
